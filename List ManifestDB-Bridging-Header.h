8a73115569d45b6920f004e8d8cec4836c7669f0
Erich Küster <erich.kuester@arcor.de>
2016-10-06 11:27:03 +0200

    First Publishing
    last local functional copy

---
+++ List ManifestDB-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  List ManifestDB-Bridging-Header.h
+//  List ManifestDB
+//
+// - go to "Build Settings" and find "Objective-C Bridging Header." Use the search bar to find it quickly.
+// - double-click and type "List ManifestDB-Bridging-Header.h" with the quotes
+// - go to "Build Phases," "Link Binary With Libraries," and add libsqlite3.0.dylib
+//
+//  Created by Erich Küster on October 3, 2016
+
+#include <sqlite3.h>
---
+++ List ManifestDB/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  List ManifestDB
+//
+//  Created by Erich Küster on October 3, 2016
+//  Copyright © 2016 Erich Küster. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        // application should terminate
+        return true
+    }
+
+}
